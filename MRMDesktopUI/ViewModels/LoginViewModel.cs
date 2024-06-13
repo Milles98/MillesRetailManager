2e969fbb752658f0729c7cc081c0c49cabd8cdb1
Mille Elfver <mille.elfver98@gmail.com>
2024-06-13 10:36:36 +0200

    Enhance LoginViewModel with Caliburn.Micro
    Updated `LoginViewModel.cs` to utilize Caliburn.Micro for improved MVVM pattern support, including namespace additions and inheritance changes. Implemented `UserName` and `Password` properties with reactive property change notifications. Adjusted `LoginView.xaml` to ensure proper data binding by aligning `x:Name` attributes with view model property names, facilitating two-way data binding and dynamic UI updates.

--- MRMDesktopUI/ViewModels/LoginViewModel.cs
+++ MRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace MRMDesktopUI.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : Screen
     {
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
+
+
     }
 }
