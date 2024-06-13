6a1ea5f434b8b80f01c44c6876fcf0566fd648c1
Mille Elfver <mille.elfver98@gmail.com>
2024-06-13 13:05:42 +0200

    Add APIHelper class for HTTP requests
    - Introduced a new APIHelper class in the MRMDesktopUI project under the Helpers directory, including its addition to the MRMDesktopUI.csproj file.
    - The APIHelper class, residing in the MRMDesktopUI.Helpers namespace, is equipped with necessary using directives for handling HTTP requests and asynchronous operations.
    - Defined a public HttpClient property named ApiClient within the APIHelper class for executing HTTP requests.
    - Implemented a constructor in the APIHelper class for initializing the ApiClient property through the InitializeClient method, which configures the HttpClient instance for JSON response acceptance.
    - Added an Authenticate method to the APIHelper class, designed to prepare authentication credentials, though it currently lacks the functionality to perform actual authentication or HTTP requests.

---
+++ MRMDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRMDesktopUI.Helpers
+{
+    public class APIHelper
+    {
+        public HttpClient ApiClient { get; set; }
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public void Authenticate(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password),
+            });
+        }
+    }
+}
