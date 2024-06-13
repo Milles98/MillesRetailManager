38b2e1f6a1f32a8d93555f5825ed763851df48d0
Mille Elfver <mille.elfver98@gmail.com>
2024-06-13 18:00:52 +0200

    Add UserController and update project file
    - Added UserController.cs in Controllers directory with authorization and a GetById method for fetching user data based on ID.
    - Updated MRMDataManager.csproj to include UserController.cs in the build, added a folder directive for user views, and added a project reference to MRMDataManager.Library.csproj for shared resources.
    - Enhanced UserController with necessary namespaces for ASP.NET Identity, data access, and model interaction.
    - Implemented GetById method in UserController to retrieve user details using the user's ID through ASP.NET Identity and UserData class.

---
+++ MRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using MRMDataManager.Library.DataAccess;
+using MRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
