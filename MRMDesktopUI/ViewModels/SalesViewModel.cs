79429863fa7405ab64ec9e65386f8794d8ddfdc9
Mille Elfver <mille.elfver98@gmail.com>
2024-06-14 09:10:57 +0200

    Added cart and product management features
    - Introduced namespaces and fields to support data binding for products and cart functionalities.
    - Implemented `Products` and `Cart` properties with `NotifyOfPropertyChange` for UI updates.
    - Added `ItemQuantity`, `SubTotal`, `Total`, and `Tax` properties with placeholders and TODOs for future implementation.
    - Created `CanAddToCart` and `CanRemoveFromCart` properties with initial logic setup.
    - Added stub methods `AddToCart()` and `RemoveFromCart()` for future functionality.
    - Fixed an issue where `ItemQuantity` setter incorrectly notifies a change in `Products`.

--- MRMDesktopUI/ViewModels/SalesViewModel.cs
+++ MRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,107 @@ namespace MRMDesktopUI.ViewModels
 {
     public class SalesViewModel : Screen
     {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                //TODO replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                //TODO replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                //TODO replace with calculation
+                return "$0.00";
+            }
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                //make sure some is select
+                //make sure item quantity
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //make sure some is select
+                //make sure item quantity
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+
     }
 }
