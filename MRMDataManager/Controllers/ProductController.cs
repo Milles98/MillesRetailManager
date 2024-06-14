18f58f3589a72e4039af486628a3b5e82c579758
Mille Elfver <mille.elfver98@gmail.com>
2024-06-14 11:22:54 +0200

    Refactor APIHelper and update ProductController
    - Uncommented `[Authorize]` attribute in `ProductController.cs` to require authorization for access.
    - Refactored `apiClient` to `_apiClient` and introduced `ApiClient` property in `APIHelper.cs` for better encapsulation.
    - Updated `HttpClient` initialization and usage in `APIHelper.cs` to adhere to naming conventions and improve maintainability.
    - Modified HTTP request headers setup in `APIHelper.cs` to use the `_apiClient` field.
    - Added `ProductEndpoint.cs` to `MRMDesktopUI.Library.csproj` for future development on product endpoints.
    - Created an empty `ProductEndpoint` class in `MRMDesktopUI.Library.Api` as a setup for future implementations.

--- MRMDataManager/Controllers/ProductController.cs
+++ MRMDataManager/Controllers/ProductController.cs
@@ -10,7 +10,7 @@ using System.Web.Http;
 
 namespace MRMDataManager.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class ProductController : ApiController
     {
         public List<ProductModel> Get()
