492f667959aef8f378a3ac92eae6fe742d196d74
Mille Elfver <mille.elfver98@gmail.com>
2024-06-14 11:13:35 +0200

    Enhanced product data management and access
    - Updated MRMData.sqlproj to include a new stored procedure `spProduct_GetAll.sql` for fetching product data.
    - Modified the `Product` table schema by adding a `QuantityInStock` column to track stock levels.
    - Added `ProductData.cs` and `ProductModel.cs` to the MRMDataManager.Library project for data access and model definition.
    - Introduced a new `ProductController.cs` in the MRMDataManager web project for handling product-related HTTP requests.
    - Implemented a new data access class `ProductData` with a method `GetProducts` to retrieve product information using the `spProduct_GetAll` stored procedure.
    - Created a `ProductModel` class to represent the product data structure, including the new `QuantityInStock` field.
    - Added authentication requirement to `ProductController` with the `[Authorize]` attribute and implemented a `Get` method for product data retrieval.

---
+++ MRMDataManager/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using MRMDataManager.Library.DataAccess;
+using MRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MRMDataManager.Controllers
+{
+    //[Authorize]
+    public class ProductController : ApiController
+    {
+        public List<ProductModel> Get()
+        {
+            ProductData data = new ProductData();
+
+            return data.GetProducts();
+        }
+    }
+}
