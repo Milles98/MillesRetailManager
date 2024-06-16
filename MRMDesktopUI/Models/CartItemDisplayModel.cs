70efc3883c702dee9a47c9c856504ff661a9d9ba
Mille Elfver <mille.elfver98@gmail.com>
2024-06-16 08:01:50 +0200

    Enhanced CartItemDisplayModel for UI updates
    - Imported System.ComponentModel for INotifyPropertyChanged.
    - Implemented INotifyPropertyChanged in CartItemDisplayModel.
    - Modified QuantityInCart to support custom setter logic.
    - Added logic in QuantityInCart setter for UI notification.
    - Introduced CallPropertyChanged method for property changes.
    - Added PropertyChanged event for UI update notifications.

--- MRMDesktopUI/Models/CartItemDisplayModel.cs
+++ MRMDesktopUI/Models/CartItemDisplayModel.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MRMDesktopUI.Models
 {
-    public class CartItemDisplayModel
+    public class CartItemDisplayModel : INotifyPropertyChanged
     {
         public ProductDisplayModel Product { get; set; }
-        public int QuantityInCart { get; set; }
+        private int _quantityInCart;
+
+        public int QuantityInCart
+        {
+            get { return _quantityInCart; }
+            set
+            {
+                _quantityInCart = value;
+                CallPropertyChanged(nameof(QuantityInCart));
+                CallPropertyChanged(nameof(DisplayText));
+            }
+        }
+
         public string DisplayText
         {
             get
@@ -17,6 +30,12 @@ namespace MRMDesktopUI.Models
                 return $"{Product.ProductName} ({QuantityInCart})";
             }
 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void CallPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(propertyName)));
         }
     }
 }
