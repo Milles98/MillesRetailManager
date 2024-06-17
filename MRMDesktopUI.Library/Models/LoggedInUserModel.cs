db3d3cb8fd396136a6db3f9979d93bca8ffc9f18
Mille Elfver <mille.elfver98@gmail.com>
2024-06-17 07:26:02 +0200

    Enhanced user login and logout flow
    - Added `LogOffUser` method in `ILoggedInUserModel` and its implementation in `LoggedInUserModel` to clear user details upon logout.
    - Updated `ShellViewModel` constructor to include `ILoggedInUserModel` dependency for accessing logged-in user information.
    - Introduced `IsAccountVisible` property in `ShellViewModel` to control the visibility of the account menu based on the user's login status.
    - Implemented logout functionality in `ShellViewModel` with a new `LogOut` method that clears user information and activates `LoginViewModel`.
    - Added event handling in `ShellViewModel` for `LogOnEvent` messages to update UI elements and activate `SalesViewModel` post-login.
    - Made miscellaneous code enhancements, including adding necessary import statements and adjusting constructor injection for improved code readability and maintainability.

--- MRMDesktopUI.Library/Models/LoggedInUserModel.cs
+++ MRMDesktopUI.Library/Models/LoggedInUserModel.cs
@@ -14,5 +14,15 @@ namespace MRMDesktopUI.Library.Models
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public DateTime CreatedDate { get; set; }
+
+        public void LogOffUser()
+        {
+            Token = "";
+            Id = "";
+            FirstName = "";
+            LastName = "";
+            EmailAddress = "";
+            CreatedDate = DateTime.MinValue;
+        }
     }
 }
--- MRMDesktopUI/ViewModels/ShellViewModel.cs
+++ MRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using MRMDesktopUI.EventModels;
+using MRMDesktopUI.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,31 @@ namespace MRMDesktopUI.ViewModels
     {
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
+        private ILoggedInUserModel _user;
+
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, ILoggedInUserModel user)
         {
             _events = events;
             _salesVM = salesVM;
-
+            _user = user;
             _events.Subscribe(this);
 
             ActivateItem(IoC.Get<LoginViewModel>());
+        }
+
+        public bool IsLoggedIn
+        {
+            get
+            {
+                bool output = false;
+
+                if (string.IsNullOrWhiteSpace(_user.Token) == false)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
         }
 
         public void ExitApplication()
@@ -28,9 +46,17 @@ namespace MRMDesktopUI.ViewModels
             TryClose();
         }
 
+        public void LogOut()
+        {
+            _user.LogOffUser();
+            ActivateItem(IoC.Get<LoginViewModel>());
+            NotifyOfPropertyChange(() => IsLoggedIn);
+        }
+
         public void Handle(LogOnEvent message)
         {
             ActivateItem(_salesVM);
+            NotifyOfPropertyChange(() => IsLoggedIn);
         }
     }
 }
