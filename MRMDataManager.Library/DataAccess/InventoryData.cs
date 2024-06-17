9cdf6b700a1e86eaa5d1deadb2f31d9a7cdc93e9
Mille Elfver <mille.elfver98@gmail.com>
2024-06-17 16:38:07 +0200

    Update inventory data handling and models
    Updated the `spInventory_GetAll` stored procedure to exclude the `Id` column, enhancing data privacy and streamlining the data structure. Added new files `InventoryData.cs` and `InventoryModel.cs` to the `MRMDataManager.Library` project, introducing a new data access layer and model class for handling inventory data more efficiently. These changes facilitate better inventory management by focusing on essential fields such as `ProductId`, `Quantity`, `PurchasePrice`, and `PurchaseDate`.

---
+++ MRMDataManager.Library/DataAccess/InventoryData.cs
@@ -0,0 +1,22 @@
+﻿using MRMDataManager.Library.Internal.DataAccess;
+using MRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRMDataManager.Library.DataAccess
+{
+    public class InventoryData
+    {
+        public List<InventoryModel> GetInventory()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<InventoryModel, dynamic>("dbo.spInventory_GetAll", new { }, "MRMData");
+
+            return output;
+        }
+    }
+}
